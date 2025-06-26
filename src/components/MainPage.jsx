4d86b6495f7b53e5b248549f3bf30820431ccf58
akshay-ken <akshaybest3120@gmail.com>
2025-06-26 16:24:05 +0530

    changed flex layout to grid layout

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -6,24 +6,24 @@ import { MdKeyboardDoubleArrowRight } from "react-icons/md";
 export function MainPage() {
   return (
     <>
-      <main className="w-full h-screen">
-        <div className="flex flex-col md:flex-row">
-          <header className="p-8">
+      <main className="w-full h-screen md:grid md:grid-cols-2">
+        {/* <div className="flex flex-col md:flex-row-reverse">
+          <header className="p-8 md:order-3">
             <img src={logoSvg} alt="" />
           </header>
-          <div>
+          <div className="order-2">
             <img
               src={bgMobile}
-              className="bg-cover bg-center md:hidden"
+              className="bg-cover bg-center md:hidden w-full"
               alt=""
             />
             <img
               src={bgDesktop}
-              className="bg-cover bg-center hidden md:block"
+              className="bg-cover bg-center hidden md:block md:h-screen"
               alt=""
             />
           </div>
-          <div className="font-one flex flex-col items-center my-14">
+          <div className="font-one mt-14 mb-6 text-center order-2">
             <h1 className="uppercase text-5xl font-light text-Desaturated-Red">
               W e ' r e
             </h1>
@@ -33,22 +33,69 @@ export function MainPage() {
             <h3 className="uppercase text-5xl font-semibold text-Dark-Grayish-Red">
               s o o n
             </h3>
-            <p className="p-6 font-medium text-Desaturated-Red text-lg text-center">
+            <p className="p-6 font-medium text-Desaturated-Red text-lg ">
+              Hello fellow shoppers! We're currently building our new fashion
+              store. Add your email below to stay up-to-date with announcements
+              and our launch deals.
+            </p>
+            <div className="relative px-8 md:flex md:flex-col self-stretch mt-6">
+              <input
+                type="text"
+                placeholder="Email Address"
+                className="outline-1 rounded-full p-4 outline-Soft-Red w-full placeholder:text-Desaturated-Red"
+              />
+              <button className="absolute top-1/2 right-8 -translate-y-1/2 rounded-full p-1 px-4 bg-linear-[135deg,hsl(0,_80%,_86%)_50%,hsl(0,_74%,_74%)]">
+                <MdKeyboardDoubleArrowRight className="size-12 fill-white" />
+              </button>
+            </div>
+          </div>
+        </div> */}
+        {/* column 1: content */}
+        <div className="p-8 md:p-18">
+          {/* Logo */}
+          <header className="mb-16">
+            <img src={logoSvg} alt="" className="w-32" />
+          </header>
+          {/* text content */}
+          <div className="text-center md:text-left">
+            <h1 className=" lg:text-6xl  tracking-[1.25rem] uppercase text-5xl font-light text-Desaturated-Red">
+              We're
+            </h1>
+            <h2 className=" lg:text-6xl tracking-[1.25rem] mt-2 uppercase text-5xl font-semibold text-Dark-Grayish-Red">
+              coming soon
+            </h2>
+            <p className="mt-6 text-base text-Desaturated-Red leading-relaxed">
               Hello fellow shoppers! We're currently building our new fashion
               store. Add your email below to stay up-to-date with announcements
               and our launch deals.
             </p>
           </div>
-          <div className="relative">
+          <form className="mt-8 relative">
             <input
-              type="text"
+              type="email"
               placeholder="Email Address"
-              className="outline-1 rounded-full p-4 mx-6 outline-Soft-Red w-full"
+              className="w-full rounded-full p-4 pl-8 border border-red-200 placeholder:text-Desaturated-Red"
             />
-            <button className="absolute top-1/2 -right-6 -translate-y-1/2 bg-Soft-Red rounded-full p-1 px-4 bg-linear-[135deg,hsl(0,_80%,_86%)_50%,hsl(0,_74%,_74%)]">
-              <MdKeyboardDoubleArrowRight className="size-12 fill-white" />
+            <button
+              type="submit"
+              className="absolute top-0 right-0 h-full px-8 rounded-full bg-gradient-to-br from-red-300 to-red-400 shadow-lg shadow-red-200 hover:scale-110 transition-transform"
+            >
+              <MdKeyboardDoubleArrowRight className="size-10 fill-white" />
             </button>
-          </div>
+          </form>
+        </div>
+        {/* hero image */}
+        <div>
+          <img
+            src={bgMobile}
+            className="bg-cover bg-center md:hidden w-full"
+            alt=""
+          />
+          <img
+            src={bgDesktop}
+            className="bg-cover bg-center w-full h-screen hidden md:block"
+            alt=""
+          />
         </div>
       </main>
     </>
